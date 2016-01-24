1370522ee23d67d9419423c3a1b2d13af7020a65
Jônatas Davi Paganini <jonatasdp@gmail.com>
2016-01-23 22:58:54 -0200

    adding simple csv

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123140702) do
+ActiveRecord::Schema.define(version: 20160124000335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 20160123140702) do
     t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "lead_imports", force: :cascade do |t|
+    t.string   "file"
+    t.integer  "leads_imported"
+    t.integer  "leads_updated"
+    t.string   "process_status"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "lead_infos", force: :cascade do |t|
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,14 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+Dir[Rails.root.join("test/factories/*.rb")].each{|e| require e}
 
-  # Add more helper methods to be used by all tests here...
+class Minitest::Unit::TestCase
+  include FactoryGirl::Syntax::Methods
+end
+class Minitest::Spec
+  include FactoryGirl::Syntax::Methods
+end
+class ActiveSupport::TestCase
+  include FactoryGirl::Syntax::Methods
 end
